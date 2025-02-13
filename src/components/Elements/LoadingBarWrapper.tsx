3a2f0fbd68c6c0fd5619d9f3d27943a0d45de73b
Jer403 <jeremiwtfxd@gmail.com>
2025-02-12 23:06:16 -0500

    Changed loader colors

--- src/components/Elements/LoadingBarWrapper.tsx
+++ src/components/Elements/LoadingBarWrapper.tsx
@@ -82,3 +82,67 @@ export function LoadingBarWrapper() {
   </div>
 </div>; */
 }
+
+{
+  /* <svg className="pl" width="240" height="240" viewBox="0 0 240 240">
+  <circle
+    className="pl__ring pl__ring--a"
+    cx="120"
+    cy="120"
+    r="105"
+    fill="none"
+    stroke="#000"
+    stroke-width="20"
+    stroke-dasharray="0 660"
+    stroke-dashoffset="-330"
+    stroke-linecap="round"
+  ></circle>
+  <circle
+    className="pl__ring pl__ring--b"
+    cx="120"
+    cy="120"
+    r="35"
+    fill="none"
+    stroke="#000"
+    stroke-width="20"
+    stroke-dasharray="0 220"
+    stroke-dashoffset="-110"
+    stroke-linecap="round"
+  ></circle>
+  <circle
+    className="pl__ring pl__ring--c"
+    cx="85"
+    cy="120"
+    r="70"
+    fill="none"
+    stroke="#000"
+    stroke-width="20"
+    stroke-dasharray="0 440"
+    stroke-linecap="round"
+  ></circle>
+  <circle
+    className="pl__ring pl__ring--d"
+    cx="155"
+    cy="120"
+    r="70"
+    fill="none"
+    stroke="#000"
+    stroke-width="20"
+    stroke-dasharray="0 440"
+    stroke-linecap="round"
+  ></circle>
+</svg>; */
+}
+
+{
+  /* <div className="w-28 h-28 border-8 text-indigo-500 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-indigo-500 rounded-full">
+  <svg
+    className="w-16 h-16 text-indigo-500 animate-ping"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <circle cx="12" cy="12" r="10" />
+  </svg>
+</div>; */
+}
